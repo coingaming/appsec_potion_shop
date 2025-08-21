6a47e6a6e8f6168e6c8e2b760dca59b9ee29322c
Ivan Giacomelli <ivan.giacomelli@yolo.com>
2025-08-21 10:23:40 +0300

    test for smithy new vuln

--- lib/carafe_web/router.ex
+++ lib/carafe_web/router.ex
@@ -102,6 +102,8 @@ defmodule CarafeWeb.Router do
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    post "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    
   end
 
   scope "/", CarafeWeb do
